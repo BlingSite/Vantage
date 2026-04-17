4b36296b3b404bca901b461efc5cde09517700b2
Liong Ng <lng@axon.com>
2026-04-16 19:15:23 -0700

    add email login

---
+++ src/app/auth/confirm/route.js
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import { createClient } from "@/lib/supabase/server";
+
+export async function GET(request) {
+  const { searchParams, origin } = new URL(request.url);
+  const token_hash = searchParams.get("token_hash");
+  const type = searchParams.get("type");
+  const next = searchParams.get("next") ?? "/";
+
+  if (token_hash && type) {
+    const supabase = await createClient();
+    const { error } = await supabase.auth.verifyOtp({ type, token_hash });
+
+    if (!error) {
+      const forwardedHost = request.headers.get("x-forwarded-host");
+      const isLocal = process.env.NODE_ENV === "development";
+
+      if (isLocal) return NextResponse.redirect(`${origin}${next}`);
+      if (forwardedHost) {
+        const proto = request.headers.get("x-forwarded-proto") ?? "https";
+        return NextResponse.redirect(`${proto}://${forwardedHost}${next}`);
+      }
+      return NextResponse.redirect(`${origin}${next}`);
+    }
+  }
+
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+}
